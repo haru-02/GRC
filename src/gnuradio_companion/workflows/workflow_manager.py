09630ce63ddf2613b0da61b9f81de7912491928a
haru-02 <rahulbalaji78@gmail.com>
2023-07-30 09:47:38 +0530

    getting workflow parameters from workflow_manager to options

--- src/gnuradio_companion/workflows/workflow_manager.py
+++ src/gnuradio_companion/workflows/workflow_manager.py
@@ -1,5 +1,5 @@
 import logging
-from collections import namedtuple
+from collections import ChainMap
 
 from . import (
         Messages, Constants,
@@ -10,8 +10,9 @@ logger = logging.getLogger(__name__)
 
 class WorkflowManager(Element):
 
-    workflow_labels=[]
-    # workflow_parameters = namedtuple('workflow_parameters', ['label', 'id', 'dtype', 'default', 'hide', 'options', 'option_labels'])
+    workflow_labels= []
+    workflow_ids = []
+    param_list = ChainMap()
 
     def load_workflow_description(self, data, filepath):
         """parse the .workflow.yml file and add it to the list"""
@@ -19,25 +20,16 @@ class WorkflowManager(Element):
         log.debug('loading %s', filepath)
         
         label = data['label']
+        workflow_id = data['id']
 
         for l in workflow_labels:
             if l == label:
                 log.error('file already parsed')
             else
-                self.checkworkflow_path.append(label)
+                self.workflow_labels.append(label)
+                self.workflow_ids.append(workflow_id)
 
         doc = data['description']
         flags = data['flags']
-        params = data['parameters']
-        # parameters = []
-
-        # for a in params:
-        #     S = workflow_parameters()
-        #     S.label = a['label']
-        #     S.id = a['id']
-        #     S.dtype = a['dtype']
-        #     S.default = a['default']
-        #     if a['hide']: S.hide = a['hide']
-        #     if a['option']: S.option = a['option']
-        #     if a['option_labels']: S.option_labels = a['option_labels']
-        #     parameters.append(S)
+        new_param = {workflow_id: data['parameters']}
+        param_list = param_list.new_child(new_param)
