4b27242470172c5d3bad31a97fddea3245eface4
haru-02 <rahulbalaji78@gmail.com>
2023-08-03 15:23:03 +0530

    refactoring templates

--- src/gnuradio_companion/core/blocks/options.py
+++ src/gnuradio_companion/core/blocks/options.py
@@ -8,6 +8,7 @@ from . import Block, register_build_in
 from ._build import build_params
 from ._templates import MakoTemplates
 from ._flags import Flags
+from ...workflows.workflow_manager import WorkflowManager
 
 templates = MakoTemplates()
 
@@ -15,8 +16,9 @@ templates = MakoTemplates()
 class Options(Block):
     
     def __init__(self):
-        workflow_params = build_params(WorkflowManager.param_list[self.key], have_inputs=True, have_outputs=True, flags=Block.flags, block_id=key)
-        self.parameters.update(workflow_params)       
+        workflow_params = build_params(WorkflowManager.param_list[self.key], have_inputs=True, have_outputs=True, flags=Block.flags, block_id=self.key)
+        self.parameters.update(workflow_params) 
+        print(self.parameters.maps())
 
     key = 'options'
     label = 'options'
@@ -52,132 +54,6 @@ class Options(Block):
                      default='python',
                      options=[], 
                      option_labels=[]),
-               
-               #  dict(label='Output Language', 
-               #       id='output_language',
-               #       dtype='enum', 
-               #       default='python', 
-               #       options=['python', 'cpp'], 
-               #       option_labels=['Python', 'C++']),
-
-               #  dict(label='Generate Options',
-               #       id='generate_options',
-               #       dtype='enum',
-               #       default='qt_gui',
-               #       option=['qt_gui, bokeh_gui, no_gui, hb, hb_qt_gui'],
-               #       option_labels=['QT GUI, Bokeh GUI, No GUI, Heir Block, Heir Block (QT GUI)']),
-
-               #  dict(label='Linking',
-               #       id='gen_linking',
-               #       dtype='enum',
-               #       default='dynamic',
-               #       options=['dynamic', 'static'],
-               #       options_labels=['Dynamic', 'Static'],
-               #       hide='all'),
-
-               #  dict(label='Generate CmakeLists.txt',
-               #       id='gen_cmake',
-               #       dtype='enum',
-               #       default='On',
-               #       options=['On', 'Off'],
-               #       hide="${ ('part' if output_language == 'cpp' else 'all') }"),
-
-               #  dict(label='CMake options',
-               #       id='cmake_opt',
-               #       dtype='string',
-               #       default='',
-               #       hide="${ ('part' if output_language == 'cpp' else 'all') }"),
-
-               #  dict(label='Category',
-               #       id='category',
-               #       dtype='string',
-               #       default='[GRC Hier Blocks]',
-               #       hide="${ ('none' if generate_options.startswith('hb') else 'all') }"),
-
-               #  dict(label='Run Options',
-               #       id='run_options',
-               #       dtype='enum',
-               #       default='prompt',
-               #       options=['run', 'prompt'],
-               #       option_labels=['Run to Completion', 'Prompt for Exit'],
-               #       hide="${ ('none' if generate_options == 'no_gui' else 'all') }"),
-                
-               #  dict(label='Widget Placement',
-               #       id='placement',
-               #       dtype='int_vector',
-               #       default=[0,0],
-               #       hide="${ ('part' if generate_options == 'bokeh_gui' else 'all') }"),
-
-               #  dict(label='Window size',
-               #       id='window_size',
-               #       dtype='int_vector',
-               #       default=[1000,1000],
-               #       hide="${ ('part' if generate_options == 'bokeh_gui' else 'all') }"),
-
-               #  dict(label='Sizing Mode',
-               #       id='sizing_mode',
-               #       dtype='enum',
-               #       default='fixed',
-               #       options=['fixed', 'stretch_both', 'scale_width', 'scale_height', 'scale_both'],
-               #       option_labels=['Fixed','Stretch Both','Scale Width','Scale Height', 'Scale Both'],
-               #       hide="${ ('part' if generate_options == 'bokeh_gui' else 'all') }"),
-
-               #  dict(label='Run',
-               #       id='run',
-               #       dtype='bool',
-               #       default='True',
-               #       options=['True', 'False'],
-               #       option_labels=['Autostart', 'Off'],
-               #       hide="${ ('all' if generate_options not in ('qt_gui', 'bokeh_gui') else ('part' if run else 'none')) }"),
-
-               #  dict(label='Max Number of Output',
-               #       id='max_nouts',
-               #       dtype='int',
-               #       default='0',
-               #       hide="${ ('all' if generate_options.startswith('hb') else ('none' if max_nouts else 'part')) }"),
-
-               #  dict(label='Realtime Scheduling',
-               #       id='realtime_scheduling',
-               #       dtype='enum',
-               #       options=['', '1'],
-               #       option_labels=['Off', 'On'],
-               #       hide="${ ('all' if generate_options.startswith('hb') else ('none' if realtime_scheduling else 'part')) }"),
-
-               #  dict(label='QSS Theme',
-               #       id='qt_qss_theme',
-               #       dtype='file_open',
-               #       hide="${ ('all' if generate_options != 'qt_gui' else ('none' if qt_qss_theme else 'part')) }"),
-
-               #  dict(label='Thread-safe setters',
-               #       id='thread_safe_setters',
-               #       category='Advanced',
-               #       dtype='enum',
-               #       options=['','1'],
-               #       option_labels=['Off', 'On'],
-               #       hide='part'),
-
-               #  dict(label='Catch Block Exceptions',
-               #       id='catch_exceptions',
-               #       category='Advanced',
-               #       dtype='enum',
-               #       options=['False', 'True'],
-               #       option_labels=['Off', 'On'],
-               #       default='True',
-               #       hide='part'),
-
-               #  dict(label='Run Command',
-               #       id='run_command',
-               #       category='Advanced',
-               #       dtype='string',
-               #       default='{python} -u {filename}',
-               #       hide="${ ('all' if generate_options.startswith('hb') else 'part') }"),
-
-               #  dict(label='Hier Block Source Path',
-               #       id='hier_block_src_path',
-               #       category='Advanced',
-               #       dtype='string',
-               #       default='.:',
-               #       hide='part')
                 ],
             have_inputs=True,
             have_outputs=True,
--- src/gnuradio_companion/workflows/workflow_manager.py
+++ src/gnuradio_companion/workflows/workflow_manager.py
@@ -1,18 +1,15 @@
 import logging
 from collections import ChainMap
 
-from . import (
-        Messages, Constants,
-        blocks, params, ports, errors, utils, schema_checker
-)
-
 logger = logging.getLogger(__name__)
 
-class WorkflowManager(Element):
+class WorkflowManager():
 
     workflow_labels= []
     workflow_ids = []
     param_list = ChainMap()
+    doc = ""
+    flags = []
 
     def load_workflow_description(self, data, filepath):
         """parse the .workflow.yml file and add it to the list"""
@@ -22,14 +19,14 @@ class WorkflowManager(Element):
         label = data['label']
         workflow_id = data['id']
 
-        for l in workflow_labels:
+        for l in self.workflow_labels:
             if l == label:
                 log.error('file already parsed')
-            else
+            else:
                 self.workflow_labels.append(label)
                 self.workflow_ids.append(workflow_id)
 
-        doc = data['description']
-        flags = data['flags']
+        self.doc = data['description']
+        self.flags = data['flags']
         new_param = {workflow_id: data['parameters']}
-        param_list = param_list.new_child(new_param)
+        self.param_list = self.param_list.new_child(new_param)
